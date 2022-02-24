1ce13fefc608f2e0f4d689090ccb66a24edaaa09
JeremyStubbs <89751851+JeremyStubbs@users.noreply.github.com>
2022-02-24 14:28:37 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests
 
-API_key = '187413fb14e195bb9d965a8e8714435c'
+
 city_name = input("Enter a city name: ")
 
 base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}"
